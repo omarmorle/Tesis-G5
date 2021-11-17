9ce17d45ea739a4355379cbe9d0066d6a8854c01
omarmorle <51431205+omarmorle@users.noreply.github.com>
2021-11-17 01:55:12 -0600

    New files added

---
+++ sesion/subirtesis.php
@@ -0,0 +1,112 @@
+<?php
+  session_start();
+  if(isset($_SESSION["email"]))
+  {
+    $email = $_SESSION["email"];
+    include("../con_db.php");
+    $consulta = "SELECT * FROM persona WHERE email = '$email'";
+    $resul = mysqli_query($conex, $consulta);
+    $filas = mysqli_fetch_row($resul);
+  }
+  else
+  {
+    header("location:./../login.php");
+  }
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>Subir</title>
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css' rel='stylesheet'>
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css' rel='stylesheet'>
+        <link href='../css/side.css' rel='stylesheet'>
+        <link rel="stylesheet" href="../css/upload.css">
+ 
+    </head>
+
+    <body oncontextmenu='return false' class='snippet-body'>
+    <body id="body-pd">
+    <header class="header" id="header">
+        <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+        <div class="header_img"> <img src="../assets/img/user.jpg" alt=""> </div>
+    </header>
+    <div class="l-navbar" id="nav-bar">
+        <nav class="nav">
+            <div> <a href="./index.php" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">Inicio</span> </a>
+                <div class="nav_list"> 
+                    <a href="./subirtesis.php" class="nav_link"> <i class='bx bx-upload'></i> <span class="nav_name">Sube Tesis</span> </a> 
+                    <a href="#" class="nav_link"> <i class='bx bx-search-alt'></i> <span class="nav_name">Buscar Tesis</span> </a> 
+                    <a href="#" class="nav_link"> <i class='bx bxs-book'></i> <span class="nav_name">Ve tus Tesis</span> </a> 
+                    <a href="#" class="nav_link"> <i class='bx bxs-time'></i> <span class="nav_name">Ultimas Tesis</span> </a> 
+            </div> <a href="./cerrarSesion.php?nombreSesion=email" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">Cerrar sesión</span> </a>
+        </nav>
+    </div>
+
+    <!--Container Main start-->
+    <section>
+        <form action="upload_pdf.php" method="POST" enctype="multipart/form-data">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="form-group">
+                  <label class="control-label">Titulo de tesis</label>
+                  <input type="text" class="form-control" name="titulo" placeholder="Titulo de tesis" required>
+                  <label class="control-label">Escuela de procedencia</label>
+                  <input type="text" class="form-control" name="escuela" placeholder="Escuela de procedencia" required>
+                  <label class="control-label">Numero de boleta 1</label>
+                  <input type="text" class="form-control" name="numeroBoleta1" placeholder="Ingresa tu numero de boleta" required>
+                  <label class="control-label">Numero de boleta 2</label>
+                  <input type="text" class="form-control" name="numeroBoleta2" placeholder="Ingresa tu numero de boleta" >
+                  <label class="control-label">Numero de boleta 3</label>
+                  <input type="text" class="form-control" name="numeroBoleta3" placeholder="Ingresa tu numero de boleta" >
+                  <label class="control-label">Numero de boleta 4</label>
+                  <input type="text" class="form-control" name="numeroBoleta4" placeholder="Ingresa tu numero de boleta" >
+                  <label class="control-label">Numero de empleado del director 1</label>
+                  <input type="text" class="form-control" name="numeroEmpleado1" placeholder="Ingresa tu numero de empleado" required>
+                  <label class="control-label">Numero de empleado del director 2</label>
+                  <input type="text" class="form-control" name="numeroEmpleado2" placeholder="Ingresa tu numero de empleado" >
+                  <br>
+                  <div class="preview-zone hidden">
+              <div class="box box-solid">
+                <div class="box-header with-border">
+                  <div><b>Vista Previa</b></div>
+                  <div class="box-tools pull-right">
+                  </div>
+                </div>
+                <div class="box-body"></div>
+              </div>
+            </div>
+            <div class="dropzone-wrapper">
+              <div class="dropzone-desc">
+                <i class="glyphicon glyphicon-download-alt"></i>
+                <p>Elige o arrastra tu tesis dentro del recuadro (Archivo PDF).</p>
+              </div>
+              <input type="file" name="img_logo" class="dropzone">
+            </div>
+          </div>
+        </div>
+      </div>
+       
+            <div class="row">
+              <div class="col-md-12">
+                <button type="submit" class="btn btn-primary pull-right">Subir</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </section>
+    
+    <!--Container Main end-->
+    
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js'></script>
+        <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js'></script>
+        <script type='text/javascript' src='../js/sidevar.js'></script>
+        <script type='text/javascript' src='../js/upload.js'></script>
+                               
+    </body>
+</html>
---
+++ sesion/upload_pdf.php
@@ -0,0 +1,63 @@
+<?php
+
+  session_start();
+  if(isset($_SESSION["email"]))
+  {
+    $email = $_SESSION["email"];
+    include("../con_db.php");
+    $consulta = "SELECT * FROM persona WHERE email = '$email'";
+    $resul = mysqli_query($conex, $consulta);
+    $filas = mysqli_fetch_row($resul);
+  }
+  else
+  {
+    header("location:./../login.php");
+  }
+
+
+//recibir parametros del formulario de subir tesis y guardarlos en variables
+$titulo = $_POST['titulo'];
+$escuela = $_POST['escuela'];
+$numeroBoleta1 = $_POST['numeroBoleta1'];
+$numeroBoleta2 = $_POST['numeroBoleta2'];
+$numeroBoleta3 = $_POST['numeroBoleta3'];
+$numeroBoleta4 = $_POST['numeroBoleta4'];
+$numeroEmpleado1 = $_POST['numeroEmpleado1'];
+$numeroEmpleado2 = $_POST['numeroEmpleado2'];
+
+//get the name of pdf file
+$pdf_name = $_FILES['documento']['name'];
+
+//print the name of pdf file
+echo $pdf_name;
+
+if(isset($_FILES['documento']) && $_FILES['documento']['type']=='application/pdf'){
+	move_uploaded_file ($_FILES['documento']['tmp_name'] , '../tesis/'.$_FILES['documento']['name']);
+}
+
+//obtener la direccion actual
+$url = "http://".$_SERVER['HTTP_HOST']."/niws/tesis/".$pdf_name;
+
+//insertar en la tabla tesis
+$consulta = "INSERT INTO tesis(boleta1, boleta2, boleta3, boleta4, profe1, profe2, link) VALUES ('$numeroBoleta1', '$numeroBoleta2', '$numeroBoleta3', '$numeroBoleta4', '$numeroEmpleado1', '$numeroEmpleado2', '$url')";
+$resultado = mysqli_query($conex,$consulta);
+if ($resultado) {
+    ?> 
+    <script type="text/javascript">
+        window.location.href = "./index.php";
+    </script>
+    <?php
+} else {
+    ?> 
+    echo'<script languaje="javascript">alert("Mori");</script>';
+    <h3 class="bad">¡Ups ha ocurrido un error!</h3>
+    <?php
+}
+
+//mandar alerta de que se subio correctamente la teesis con el titulo $titulo
+echo "<script>alert('Se subio correctamente la tesis con el nombre $url');</script>";
+
+//redireccionar a la pagina de inicio
+echo '<script>window.location.href="./index.php";</script>';
+
+?>
